4104fb6ab3caaf6064609f23577ad65e63b759c7
Craig J. Bass <1889973+craigjbass@users.noreply.github.com>
2020-04-12 12:05:03 +0100

    Add some basic unit tests around the command building.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,106 @@
 extern crate clap;
 
+use std::option::Option;
 use std::process::{Command, Stdio};
 use clap::{App, Arg};
+
+struct BashRunner {}
+impl Runner for BashRunner {
+    fn run_command(&mut self, command: String) {
+        let mut child = Command::new("bash")
+            .stdin(Stdio::inherit())
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .arg("-c")
+            .arg(command)
+            .spawn()
+            .expect("Could not find binary 'bash'.");
+        
+        child.wait().expect("Command failed.");
+    }
+}
 
 fn main() {
     let matches = App::new("aws-profile-vault")
         .version("Cherrytree")
         .author("Craig J. Bass <craig@madetech.com>")
         .about("Let's you run scripts that use aws-profile when you only have aws-vault.")
-        .arg(Arg::with_name("profile").short("p").takes_value(true).required(true).value_name("PROFILE").help("The AWS profile to use."))
-        .arg(Arg::with_name("command").required(true).multiple(true))
+        .arg(Arg::with_name("profile")
+             .short("p")
+             .long("profile")
+             .takes_value(true)
+             .required(true)
+             .value_name("PROFILE")
+             .help("The AWS profile to use."))
+        .arg(Arg::with_name("command")
+             .required(true)
+             .multiple(true))
         .get_matches();
 
-    let mut child = Command::new("bash")
-        .stdin(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .arg("-c")
-        .arg(format!("{}{}{}{}", "aws-vault exec ", matches.value_of("profile").unwrap(), " -- ", matches.values_of("command").map(|vals| vals.collect::<Vec<_>>()).unwrap().join(" ")))
-        .spawn()
-        .expect("Could not find binary 'bash'.");
-    
+    execute(
+        &mut BashRunner {},
+        UserRequest {
+            parameter_profile: matches.value_of("profile").map(str::to_string),
+            parameter_command: matches.values_of_lossy("command")
+        }
+    )
+}
 
-    child.wait().expect("Command failed.");
+trait Runner {
+  fn run_command(&mut self, command: String);
+}
+
+struct UserRequest {
+    parameter_profile: Option<String>,
+    parameter_command: Option<Vec<String>>
+}
+
+fn execute(runner: &mut Runner, request: UserRequest) {
+    runner.run_command(format!("{}{}{}{}", "aws-vault exec ", request.parameter_profile.unwrap(), " -- ", request.parameter_command.unwrap().join(" ")));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct RunnerSpy {
+        command: Option<String>
+    }
+    impl Runner for RunnerSpy {
+        fn run_command(&mut self, command: String) {
+            self.command = Some(command);
+        }
+    }
+
+    #[test]
+    fn can_run_aws_vault_1() {
+        let mut spy = RunnerSpy {
+            command: None
+        };
+        execute(
+            &mut spy,
+            UserRequest {
+                parameter_profile: Some("sandbox").map(String::from),
+                parameter_command: Some(vec!("aws", "s3", "ls").into_iter().map(String::from).collect())
+            }
+        );
+
+        assert_eq!(spy.command, Some(String::from("aws-vault exec sandbox -- aws s3 ls")));
+    }
+
+    #[test]
+    fn can_run_aws_vault_2() {
+        let mut spy = RunnerSpy {
+            command: None
+        };
+        execute(
+            &mut spy,
+            UserRequest {
+                parameter_profile: Some("production").map(String::from),
+                parameter_command: Some(vec!("env").into_iter().map(String::from).collect())
+            }
+        );
+
+        assert_eq!(spy.command, Some(String::from("aws-vault exec production -- env")));
+    }
 }
